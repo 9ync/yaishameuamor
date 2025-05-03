45f44dc36de6ce1f4dc598e1aea1120db8eaf7d8
ync <johngother09@gmail.com>
2025-05-03 17:03:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,3 @@ function liberar() {
   document.getElementById('bloqueio').style.display = 'none';
   document.getElementById('conteudo').classList.remove('oculto');
 }
-
-const playPauseBtn = document.getElementById('playPauseBtn');
-const audioPlayer = document.getElementById('audioPlayer');
-const volumeControl = document.getElementById('volumeControl');
-
-let isPlaying = false;
-
-playPauseBtn.addEventListener('click', function () {
-  if (isPlaying) {
-    audioPlayer.pause();
-    playPauseBtn.textContent = 'Play';
-  } else {
-    audioPlayer.play();
-    playPauseBtn.textContent = 'Pause';
-  }
-  isPlaying = !isPlaying;
-});
-
-volumeControl.addEventListener('input', function () {
-  audioPlayer.volume = volumeControl.value / 100;
-});
