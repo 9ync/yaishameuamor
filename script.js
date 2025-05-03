2c3ccf6921c97fce5535c0fc2079355696a0d077
ync <johngother09@gmail.com>
2025-05-03 15:23:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function mostrarMensagem() {
-  document.getElementById("mensagem").innerText =
-    "minha garota...";
-}
-
-function liberarSite() {
-  document.getElementById("entrada").style.display = "none";
-  const conteudo = document.getElementById("conteudoSite");
-  conteudo.classList.remove("oculto");
-  conteudo.classList.add("revelado");
+function liberar() {
+  document.getElementById('bloqueio').style.display = 'none';
+  document.getElementById('conteudo').classList.remove('oculto');
 }
