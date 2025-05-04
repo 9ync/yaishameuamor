94316fe3bd04b749a1fe3c40fd2e8fddea9daa35
ync <johngother09@gmail.com>
2025-05-03 21:58:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function liberar() {
   document.getElementById('bloqueio').style.display = 'none';
   document.getElementById('conteudo').classList.remove('oculto');
-
-  // Reproduz música de fundo
+  
   const audio = new Audio('musica.mp3');
   audio.loop = true;
   audio.volume = 0.5;
