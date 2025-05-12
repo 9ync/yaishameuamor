63bfb130fcf5437225e55f47be294e954903fd72
ync <johngother09@gmail.com>
2025-05-12 19:33:17 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function liberar() {
-  document.getElementById('bloqueio').style.display = 'none';
-  const conteudo = document.getElementById('conteudo');
-  conteudo.style.display = 'flex';
-  conteudo.classList.add('fade-in');
-}
